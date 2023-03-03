51f91db9e96ecfb949cc92dd3f8503bcdaa10e22
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-03-03 14:42:08 +0000

    fix: fix rootDir for dev server programmatic usage

--- packages/nuxi/src/run.ts
+++ packages/nuxi/src/run.ts
@@ -5,6 +5,7 @@ import { commands } from './commands'
 export async function runCommand (command: string, argv = process.argv.slice(2)) {
   const args = mri(argv)
   args.clear = false // used by dev
+  args.rootDir = args._[0]
   const cmd = await commands[command as Command]() as NuxtCommand
   if (!cmd) {
     throw new Error(`Invalid command ${command}`)
